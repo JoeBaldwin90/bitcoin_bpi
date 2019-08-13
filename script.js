115f63395e8f0e0616d4ce7a151a8a24704d525d
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-08-13 16:01:03 +0100

    Use link attr to change BPI displayed

--- script.js
+++ script.js
@@ -3,15 +3,29 @@ const url = "https://api.coindesk.com/v1/bpi/currentprice.json"
 
 // Elements
 const priceEl = document.querySelector("h1")
+const navLinks = document.querySelectorAll("nav a")
+
+let currency = "GBP"
 
 const checkPrice = function () {
   // Fetch
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      priceEl.innerHTML = data.bpi.USD.rate_float.toFixed(1)
+      priceEl.innerHTML = data.bpi[currency].rate_float.toFixed(1)
     })
 }
 
 // Pull latest BPI data on page load
 checkPrice()
+
+// On click, change the BPI currency based on the link's data-currency
+navLinks.forEach(link => {
+  // Add event listener for each link
+  link.addEventListener("click", function () {
+    // Get data attribute for clicked link
+    currency = this.getAttribute("data-currency")
+    // Check price for new currency
+    checkPrice()
+  })
+})
