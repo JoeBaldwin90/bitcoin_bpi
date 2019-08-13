669e2bf277bc0b93d0b6e487cbf2f30b21df3d31
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-08-13 15:46:20 +0100

    Display the USD BPI value in the main H1 element once the page loads

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-fetch("")
-  .then(response => response.json())
-  .then(json => {
-    data = json
-  })
+// Current bitcoin price API endpoint
+const url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+
+// Elements
+const priceEl = document.querySelector("h1")
+
+const checkPrice = function () {
+  // Fetch
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      priceEl.innerHTML = data.bpi.USD.rate_float.toFixed(1)
+    })
+}
+
+// Pull latest BPI data on page load
+checkPrice()
